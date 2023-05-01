ca94ab46fe499b8f182cff1d910346bf3e8f6305
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-05-01 12:24:29 -0300

    Configuração das propriedades e relacionamentos usando a fluent api

--- ApiCatalogoMinimalAPI/ApiCatalogoMinimalAPI/Context/AppDbContext.cs
+++ ApiCatalogoMinimalAPI/ApiCatalogoMinimalAPI/Context/AppDbContext.cs
@@ -7,6 +7,31 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Produto>? Produtos { get; set; }
     public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+        mb.Entity<Categoria>().Property(c => c.Nome)
+                              .HasMaxLength(100)
+                              .IsRequired();
+        mb.Entity<Categoria>().Property(c => c.Descricao)
+                              .HasMaxLength(150);
+
+        mb.Entity<Produto>().HasKey(p => p.ProdutoId);
+        mb.Entity<Produto>().Property(p => p.Nome)
+                            .HasMaxLength(100)
+                            .IsRequired();
+        mb.Entity<Produto>().Property(p => p.Descricao)
+                            .HasMaxLength(100);
+        mb.Entity<Produto>().Property(p => p.Imagem)
+                            .HasMaxLength(250);
+        mb.Entity<Produto>().Property(p => p.Preco)
+                            .HasPrecision(14, 2);
+
+        mb.Entity<Produto>().HasOne(c => c.Categoria)
+                            .WithMany(c => c.Produtos)
+                            .HasForeignKey(c => c.CategoriaId);
+    }
 }
