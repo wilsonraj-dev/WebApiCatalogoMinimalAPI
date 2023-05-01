8641c6ad96ab0b869d82f0dccf0d4630421ce411
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-05-01 12:07:13 -0300

    Criação do modelo de domínio

---
+++ ApiCatalogoMinimalAPI/ApiCatalogoMinimalAPI/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiCatalogoMinimalAPI.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? Imagem { get; set; }
+    public DateTime DataCompra { get; set; }
+    public int Estoque { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
+}
