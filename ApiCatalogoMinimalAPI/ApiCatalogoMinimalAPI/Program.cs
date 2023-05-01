dfc226c6bcb937407dbb2a697a421e63f9cb6970
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-05-01 16:15:17 -0300

    Criando o endpoint para criar uma nova categoria com MapPost

--- ApiCatalogoMinimalAPI/ApiCatalogoMinimalAPI/Program.cs
+++ ApiCatalogoMinimalAPI/ApiCatalogoMinimalAPI/Program.cs
@@ -1,18 +1,26 @@
 using ApiCatalogoMinimalAPI.Context;
+using ApiCatalogoMinimalAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
                                 options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
+
+app.MapPost("/categrorias", async (Categoria categoria, AppDbContext db) =>
+{
+    db.Categorias.Add(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
